d0e1d4409a5be7d27fd1ccf94afaf1ffac7aca79
Igor Bondarev <igor.bondarev@hotmail.com>
2019-03-22 00:21:20 +0300

    Update

--- anti_ddos.py
+++ anti_ddos.py
@@ -1,7 +1,9 @@
 import time
 
 # Config:
-check_ip_switch = 1  # 1 - We check ips. 0 - if we want to turn off checking ips
+
+# 1 - We check ips. 0 - if we want to turn off checking ips
+check_ip_switch = True
 log_time = 20  # Number in seconds in which we check for number of logins
 num_softban = 20  # Number of max acceptable logins for log_time seconds
 threshold = 2
@@ -9,38 +11,55 @@ threshold = 2
 ip_filter_table = {}
 num_ips = 0
 
+OK = 0
+SOFT_BAN = 1
+NO_SPACE = 2
+
+
+# FIXME
+def clear_table():
+    for ip in list(ip_filter_table):
+        if ip_filter_table[ip][0]["STATUS"] == 1 \
+          and time.time() - ip_filter_table[ip][0]["TIME"] > log_time:
+            del ip_filter_table[ip]
+        else:
+            return 2
+
 
 def check_ip(our_ip):
     global ip_filter_table
     global num_ips
-    if check_ip_switch:
-        if num_ips == threshold:
-            clear_table()
-        elif our_ip not in ip_filter_table.keys():
-            ip_filter_table[our_ip] = [{"TIME": time.time(), "NUM_LOGINS": 1, "STATUS": 0}, ]
-            num_ips += 1
-            return 0  # LEGIT
+
+    if not check_ip_switch:
+        return OK, ""
+
+    if num_ips >= threshold:
+        clear_table()
+
+    if num_ips >= threshold:
+        return 2, "IP filer is overfilled. Please try again late"
+
+    if our_ip not in ip_filter_table.keys():
+        ip_filter_table[our_ip] = [{"TIME": time.time(),
+                                   "NUM_LOGINS": 1, "STATUS": 0}]
+        num_ips += 1
+        return OK, ""
+    else:
+        if ip_filter_table[our_ip][0]["STATUS"] == 1:
+            return SOFT_BAN, "You has been blocked for <x> minutes." \
+                              "Please try again later."
+            # TODO: return 1, time - где time это время бана, которое мы можем
+            # увеличивать
+        elif ip_filter_table[our_ip][0]["NUM_LOGINS"] + 1 > num_softban:
+            ip_filter_table[our_ip][0]["STATUS"] = 1
+            return SOFT_BAN, ""
         else:
-            if ip_filter_table[our_ip][0]["STATUS"] == 1:
-                return 1  # SOFTBAN
-                # TODO: return 1, time - где time это время бана, которое мы можем увеличивать
-            elif ip_filter_table[our_ip][0]["NUM_LOGINS"] + 1 > num_softban:
-                ip_filter_table[our_ip][0]["STATUS"] = 1
-                return 1  # SOFTBAN
+            login_time = time.time()
+
+            if login_time - ip_filter_table[our_ip][0]["TIME"] > log_time:
+                ip_filter_table[our_ip][0]["TIME"] = login_time
+                ip_filter_table[our_ip][0]["NUM_LOGINS"] = 1
+                return OK, ""  # LEGIT
             else:
-                login_time = time.time()
-                if login_time - ip_filter_table[our_ip][0]["TIME"] > log_time:
-                    ip_filter_table[our_ip][0]["TIME"] = login_time
-                    ip_filter_table[our_ip][0]["NUM_LOGINS"] = 1
-                    return 0  # LEGIT
-                else:
-                    ip_filter_table[our_ip][0]["NUM_LOGINS"] += 1
-                    return 0  # LEGIT
-
-
-def clear_table():
-    for ip in list(ip_filter_table):
-        if ip_filter_table[ip][0]["STATUS"] == 1 and time.time() - ip_filter_table[ip][0]["TIME"] > log_time:
-            del ip_filter_table[ip]
-        else:
-            return 2
+                ip_filter_table[our_ip][0]["NUM_LOGINS"] += 1
+                return OK, ""  # LEGIT
