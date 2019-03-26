aeee5d10173500baa712f63d13c5b83fd44be661
user <schubinkostya@yandex.ru>
2019-03-27 01:02:07 +0300

    Cosmetics

--- anti_ddos.py
+++ anti_ddos.py
@@ -22,11 +22,9 @@ NO_SPACE = 2
 # FIXME
 def clear_table():
     for ip in list(ip_filter_table):
-        if ip_filter_table[ip][0]["STATUS"] == 0 \
-                and time.time() - ip_filter_table[ip][0]["TIME"] > log_time:
+        if ip_filter_table[ip]["STATUS"] == 0 \
+                and time.time() - ip_filter_table[ip]["TIME"] > log_time:
             del ip_filter_table[ip]
-        else:
-            return NO_SPACE
 
 
 def check_ip(our_ip):
@@ -38,7 +36,7 @@ def check_ip(our_ip):
     login_time = time.time()
 
     if not check_ip_switch:
-        return OK, ""
+        return OK, "Website is currently disabled"
 
     if num_ips >= threshold:
         clear_table()
@@ -48,37 +46,37 @@ def check_ip(our_ip):
         return NO_SPACE, "IP filer is overfilled. Please try again late"
 
     if our_ip not in ip_filter_table.keys():
-        ip_filter_table[our_ip] = [{"TIME": time.time(),
-                                    "NUM_LOGINS": 1, "STATUS": 0, "BANNED_TIME": 0}]
+        ip_filter_table[our_ip] = {"TIME": time.time(),
+                                   "NUM_LOGINS": 1, "STATUS": 0, "BANNED_TIME": 0}
         num_ips += 1
-        return OK, ""
+        return OK, "Welcome"
 
-    if ip_filter_table[our_ip][0]["STATUS"] == 1:
-        if ip_filter_table[our_ip][0]["BANNED_TIME"] == 0 and login_time - ip_filter_table[our_ip][0]["TIME"] > log_time:
-            ip_filter_table[our_ip][0]["STATUS"] = 0
-            ip_filter_table[our_ip][0]["TIME"] = login_time
+    if ip_filter_table[our_ip]["STATUS"] == 1:
+        if ip_filter_table[our_ip]["BANNED_TIME"] == 0 and login_time - ip_filter_table[our_ip]["TIME"] > log_time:
+            ip_filter_table[our_ip]["STATUS"] = 0
+            ip_filter_table[our_ip]["TIME"] = login_time
             return OK, ""
-        elif ip_filter_table[0]["BANNED_TIME"] > login_time - ip_filter_table[our_ip][0]["TIME"]:
-            ip_filter_table[our_ip][0]["TIME"] = login_time
-            ip_filter_table[our_ip][0]["BANNED_TIME"] = ban_time_coef * (ip_filter_table[0]["BANNED_TIME"] - login_time - ip_filter_table[our_ip][0]["TIME"])
-            if ip_filter_table[our_ip][0]["BANNED_TIME"] > max_ban_time:
-                ip_filter_table[our_ip][0]["BANNED_TIME"] = max_ban_time
-            remaining_ban_time = time.strftime("%H hours, %M minutes and %S seconds. ", time.gmtime(ip_filter_table[our_ip][0]["BANNED_TIME"]))
+        elif ip_filter_table["BANNED_TIME"] > login_time - ip_filter_table[our_ip]["TIME"]:
+            ip_filter_table[our_ip]["TIME"] = login_time
+            ip_filter_table[our_ip]["BANNED_TIME"] = ban_time_coef * (ip_filter_table[our_ip]["BANNED_TIME"] - login_time - ip_filter_table[our_ip]["TIME"])
+            if ip_filter_table[our_ip]["BANNED_TIME"] > max_ban_time:
+                ip_filter_table[our_ip]["BANNED_TIME"] = max_ban_time
+            remaining_ban_time = time.strftime("%H hours, %M minutes and %S seconds. ", time.gmtime(ip_filter_table[our_ip]["BANNED_TIME"]))
             return SOFT_BAN, f"Your ban time was increased and now is {remaining_ban_time} seconds." \
                 "Please try again later."
-    elif ip_filter_table[our_ip][0]["NUM_LOGINS"] + 1 > num_softban:
-        ip_filter_table[our_ip][0]["STATUS"] = 1
-        ip_filter_table[our_ip][0]["TIME"] = login_time
-        ip_filter_table[our_ip][0]["BANNED_TIME"] = first_time_ban
+    elif ip_filter_table[our_ip]["NUM_LOGINS"] + 1 > num_softban:
+        ip_filter_table[our_ip]["STATUS"] = 1
+        ip_filter_table[our_ip]["TIME"] = login_time
+        ip_filter_table[our_ip]["BANNED_TIME"] = first_time_ban
         remaining_ban_time = time.strftime("%H hours, %M minutes and %S seconds. ", time.gmtime(first_time_ban))
         return SOFT_BAN, f"You have been blocked for {remaining_ban_time}" \
             f"If you return before this time ends, your remaining ban time will be multiplied by {ban_time_coef}. " \
             "Please try again later."
     else:
-        if login_time - ip_filter_table[our_ip][0]["TIME"] > log_time:
-            ip_filter_table[our_ip][0]["NUM_LOGINS"] = 1
-            ip_filter_table[our_ip][0]["TIME"] = login_time
+        if login_time - ip_filter_table[our_ip]["TIME"] > log_time:
+            ip_filter_table[our_ip]["NUM_LOGINS"] = 1
+            ip_filter_table[our_ip]["TIME"] = login_time
             return OK, ""
         else:
-            ip_filter_table[our_ip][0]["NUM_LOGINS"] += 1
+            ip_filter_table[our_ip]["NUM_LOGINS"] += 1
             return OK, ""
--- test_anti_ddos.py
+++ test_anti_ddos.py
@@ -1,19 +1,19 @@
-import anti_ddos
+import anti_ddos as anti
 import time
 
 
 # unit tests on pytest module
 def test_check_ip_switch_off():
-    anti_ddos.check_ip_switch = False
-    assert anti_ddos.check_ip("192.0.0.0") == (anti_ddos.OK, "")
+    anti.check_ip_switch = False
+    assert anti.check_ip("192.0.0.0") == (anti.OK, "Website is currently disabled")
 
 
 def test_check_ip_switch_on():
-    anti_ddos.check_ip_switch = True
-    assert anti_ddos.check_ip("192.0.0.1") == (anti_ddos.OK, "")
+    anti.check_ip_switch = True
+    assert anti.check_ip("192.0.0.1") == (anti.OK, "Welcome")
 
 
 def test_adding_new_ip():
-    anti_ddos.check_ip("192.0.0.2")
-    assert time.time() + 1 >= anti_ddos.ip_filter_table["192.0.0.2"][0]["TIME"]
-    assert time.time() - 1 <= anti_ddos.ip_filter_table["192.0.0.2"][0]["TIME"]
+    anti.check_ip("192.0.0.2")
+    assert time.time() + 1 >= anti.ip_filter_table["192.0.0.2"]["TIME"]
+    assert time.time() - 1 <= anti.ip_filter_table["192.0.0.2"]["TIME"]
