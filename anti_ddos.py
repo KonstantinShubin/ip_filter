5c90bd0ad3ec6d7ecb7422056e5cc59e64c1510c
user <schubinkostya@yandex.ru>
2019-03-27 14:35:10 +0300

    Cosmetics for more readability

--- anti_ddos.py
+++ anti_ddos.py
@@ -33,50 +33,52 @@ def check_ip(our_ip):
 
     # TODO: Add function metrics
 
-    login_time = time.time()
+    current_time = time.time()
 
     if not check_ip_switch:
-        return OK, "Website is currently disabled"
+        return OK, "Website is currently disabled. "
 
     if num_ips >= threshold:
         clear_table()
         num_ips = len(ip_filter_table.keys())
 
     if num_ips >= threshold:
-        return NO_SPACE, "IP filer is overfilled. Please try again late"
+        return NO_SPACE, "IP filer is overfilled. Please try again late. "
 
     if our_ip not in ip_filter_table.keys():
         ip_filter_table[our_ip] = {"TIME": time.time(),
                                    "NUM_LOGINS": 1, "STATUS": 0, "BANNED_TIME": 0}
         num_ips += 1
-        return OK, "Welcome"
+        return OK, "Welcome, new user. "
 
     if ip_filter_table[our_ip]["STATUS"] == 1:
-        if ip_filter_table[our_ip]["BANNED_TIME"] == 0 and login_time - ip_filter_table[our_ip]["TIME"] > log_time:
+        if ip_filter_table[our_ip]["BANNED_TIME"] == 0 and current_time - ip_filter_table[our_ip]["TIME"] > log_time:
             ip_filter_table[our_ip]["STATUS"] = 0
-            ip_filter_table[our_ip]["TIME"] = login_time
+            ip_filter_table[our_ip]["TIME"] = current_time
             return OK, ""
-        elif ip_filter_table["BANNED_TIME"] > login_time - ip_filter_table[our_ip]["TIME"]:
-            ip_filter_table[our_ip]["TIME"] = login_time
-            ip_filter_table[our_ip]["BANNED_TIME"] = ban_time_coef * (ip_filter_table[our_ip]["BANNED_TIME"] - login_time - ip_filter_table[our_ip]["TIME"])
+        elif ip_filter_table["BANNED_TIME"] > current_time - ip_filter_table[our_ip]["TIME"]:
+            ip_filter_table[our_ip]["TIME"] = current_time
+            ip_filter_table[our_ip]["BANNED_TIME"] = ban_time_coef * (ip_filter_table[our_ip]["BANNED_TIME"] - current_time - ip_filter_table[our_ip]["TIME"])
             if ip_filter_table[our_ip]["BANNED_TIME"] > max_ban_time:
                 ip_filter_table[our_ip]["BANNED_TIME"] = max_ban_time
             remaining_ban_time = time.strftime("%H hours, %M minutes and %S seconds. ", time.gmtime(ip_filter_table[our_ip]["BANNED_TIME"]))
             return SOFT_BAN, f"Your ban time was increased and now is {remaining_ban_time} seconds." \
-                "Please try again later."
-    elif ip_filter_table[our_ip]["NUM_LOGINS"] + 1 > num_softban:
-        ip_filter_table[our_ip]["STATUS"] = 1
-        ip_filter_table[our_ip]["TIME"] = login_time
-        ip_filter_table[our_ip]["BANNED_TIME"] = first_time_ban
-        remaining_ban_time = time.strftime("%H hours, %M minutes and %S seconds. ", time.gmtime(first_time_ban))
-        return SOFT_BAN, f"You have been blocked for {remaining_ban_time}" \
-            f"If you return before this time ends, your remaining ban time will be multiplied by {ban_time_coef}. " \
-            "Please try again later."
+                "Please try again later. "
+
     else:
-        if login_time - ip_filter_table[our_ip]["TIME"] > log_time:
+        if current_time - ip_filter_table[our_ip]["TIME"] > log_time:
             ip_filter_table[our_ip]["NUM_LOGINS"] = 1
-            ip_filter_table[our_ip]["TIME"] = login_time
-            return OK, ""
+            ip_filter_table[our_ip]["TIME"] = current_time
+            return OK, "Welcome. Haven't seen you for a while. "
         else:
             ip_filter_table[our_ip]["NUM_LOGINS"] += 1
-            return OK, ""
+            if ip_filter_table[our_ip]["NUM_LOGINS"] > num_softban:
+                ip_filter_table[our_ip]["STATUS"] = 1
+                ip_filter_table[our_ip]["TIME"] = current_time
+                ip_filter_table[our_ip]["BANNED_TIME"] = first_time_ban
+                remaining_ban_time = time.strftime("%H hours, %M minutes and %S seconds. ", time.gmtime(first_time_ban))
+                return SOFT_BAN, f"You have been blocked for {remaining_ban_time}" \
+                    f"If you return before this time ends, your remaining ban time will be multiplied by {ban_time_coef}. " \
+                    "Please try again later. "
+            else:
+                return OK, "Hi again. "
